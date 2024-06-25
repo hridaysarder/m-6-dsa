070efffb32d6c258fde7ea9dc03d322801ca6ad8
HridaySarder <hridoyaug12@gmail.com>
2024-06-26 00:31:18 +0600

    added insert_at_tail

---
+++ insert_at_tail.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Node
+{
+public:
+    int val;
+    Node *next;
+    Node(int val)
+    {
+        this->val = val;
+        this->next = NULL;
+    }
+};
+void insert_it_tail(Node *&head, int val)
+{
+    Node *newNode = new Node(val);
+    if (head == NULL)
+    {
+        head = newNode;
+        return;
+    }
+    Node *temp = head;
+    while (temp->next != NULL)
+    {
+        temp = temp->next;
+    }
+    temp->next = newNode;
+}
+void print(Node *head)
+{
+    Node *temp = head;
+    while (temp != NULL)
+    {
+        cout << temp->val << " ";
+        temp = temp->next;
+    }
+    cout << endl;
+}
+int main()
+{
+    Node *head = NULL;
+    insert_it_tail(head, 1);
+    insert_it_tail(head, 2);
+    print(head);
+    return 0;
+}
